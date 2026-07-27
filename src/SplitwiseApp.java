b21abb78079defc7554e769abf11243a70d04ac5
preethii <preethi.25bscaids@kprcas.ac.in>
2026-07-28 00:24:43 +0530

    2nd commit

--- src/SplitwiseApp.java
+++ src/SplitwiseApp.java
@@ -1,13 +1,11 @@
 public class SplitwiseApp {
-    public static void main() {
+    public static void main(String[] args) {
         System.out.println("===== Splitwise Console =====");
         System.out.println("Track shared expenses with friends.");
         System.out.println();
-        System.out.println("Ready. More features coming in the next lessons. ");
-        String payerName = "Riya";
+        System.out.println("Ready. More features coming in the next lessons.");
         double totalAmount = 500.0;
         int numFriends = 3;
-        System.out.println(payerName);
         System.out.println(totalAmount);
         System.out.println(numFriends);
     }
