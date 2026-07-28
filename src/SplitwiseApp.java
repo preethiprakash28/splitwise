bba3d9874761e55add9b9258221146593d8626f0
preethii <preethi.25bscaids@kprcas.ac.in>
2026-07-28 11:49:27 +0530

    6th commit

--- src/SplitwiseApp.java
+++ src/SplitwiseApp.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
 public class SplitwiseApp {
     static void main() {
+        Scanner sc= new Scanner(System.in);
         System.out.println("===== Splitwise Console =====");
         System.out.println("Track shared expenses with friends.");
         System.out.println();
         System.out.println("Ready. More features coming in the next lessons. ");
-        String payerName = "Riya";
-        double totalAmount = 500.0;
+        System.out.println("Enter Payer Name:");
+        String payerName = sc.nextLine();
+        System.out.println("Enter Total Amount:");
+        double totalAmount = sc.nextDouble();
         int numFrnds = 3;
         double amountPerPerson = totalAmount / numFrnds;
-        String expense = payerName + "paid ₹" + totalAmount;
+        String expense = payerName + " paid ₹" + totalAmount;
         String share = "Each person pays: ₹" + amountPerPerson;
         System.out.println(expense);
         System.out.println(share);
