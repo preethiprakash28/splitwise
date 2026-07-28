683f9cb749a7f58ec57648b871418d6f02fba850
preethii <preethi.25bscaids@kprcas.ac.in>
2026-07-28 12:25:32 +0530

    7th commit

--- src/SplitwiseApp.java
+++ src/SplitwiseApp.java
@@ -6,15 +6,38 @@ public class SplitwiseApp {
         System.out.println("Track shared expenses with friends.");
         System.out.println();
         System.out.println("Ready. More features coming in the next lessons. ");
-        System.out.println("Enter Payer Name:");
-        String payerName = sc.nextLine();
-        System.out.println("Enter Total Amount:");
-        double totalAmount = sc.nextDouble();
-        int numFrnds = 3;
-        double amountPerPerson = totalAmount / numFrnds;
-        String expense = payerName + " paid ₹" + totalAmount;
-        String share = "Each person pays: ₹" + amountPerPerson;
-        System.out.println(expense);
-        System.out.println(share);
+        boolean running = true;
+        while(running){
+            System.out.println("==== Main Menu ====");
+            System.out.println("1. Record expense");
+            System.out.println("0. Quit");
+            System.out.println("Choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch(choice) {
+                case 1 -> {
+                    System.out.println("Enter Payer Name:");
+                    String payerName = sc.nextLine();
+                    System.out.println("Enter Total Amount:");
+                    double totalAmount = sc.nextDouble();
+                    sc.nextLine();
+                    int numFrnds = 3;
+                    double amountPerPerson = totalAmount / numFrnds;
+                    String expense = payerName + " paid ₹" + totalAmount;
+                    String share = "Each person pays : ₹" + amountPerPerson;
+                    System.out.println(expense);
+                    System.out.println(share);
+                    System.out.println();
+                }
+                case 0 ->{
+                    System.out.println("Goodbye!!!!!");
+                    running = false;
+                }
+                default ->{
+                    System.out.println("Invalid choice. Try again.");
+                    System.out.println();
+                }
+            }
+        }
     }
 }
